44cbe40a8c5bd2ba7caf52c1d729ff3bdd440ccd
cougarrobotics1982@gmail.com <1982cougarrobotics>
2017-02-14 20:56:08 -0600

    Tweaked enums

--- CommonRobotics/src/org/frc1982/common/FieldPosition.java
+++ CommonRobotics/src/org/frc1982/common/FieldPosition.java
@@ -1,7 +1,28 @@
 package org.frc1982.common;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
 public enum FieldPosition {
-	ONE
-	, TWO
-	, THREE
+	ONE( "One" )
+	, TWO( "Two" )
+	, THREE( "Three" )
+	;
+	
+	public static final String TITLE = "Field Position";
+	public static SendableChooser<FieldPosition> getFieldPosition() {
+		SendableChooser<FieldPosition> fieldPosition = new SendableChooser<FieldPosition>();
+		
+		for (FieldPosition fp : FieldPosition.values()) {
+			fieldPosition.addObject( fp.getLabel(), fp );
+		}
+		
+		return fieldPosition;
+	}
+	
+	private final String label;
+	public String getLabel() { return label; }
+	
+	private FieldPosition( String label ) {
+		this.label = label;
+	}
 }
--- CommonRobotics/src/org/frc1982/common/Goal.java
+++ CommonRobotics/src/org/frc1982/common/Goal.java
@@ -1,7 +1,29 @@
 package org.frc1982.common;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
 public enum Goal {
-	DO_NOTHING
-	, BASELINE
-	, GEAR
+	DO_NOTHING( "Do Nothing" )
+	, BASELINE( "BaseLine" )
+	, GEAR( "Gear" )
+	;
+	
+	public static final String TITLE = "Goal";
+	
+	private final String label;
+	public String getLabel() { return label; }
+	
+	public static SendableChooser<Goal> getGoal() {
+		SendableChooser<Goal> goal = new SendableChooser<Goal>();
+		
+		for (Goal g : Goal.values()) {
+			goal.addObject( g.getLabel(), g );
+		}
+		
+		return goal;
+	}
+	
+	private Goal( String label ) {
+		this.label = label;
+	}
 }
